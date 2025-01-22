817c6248aef21c3d317b78f5b3539e8479fc37fc
Shahriar Kabir Tashin <122079836+shahriarkabirT@users.noreply.github.com>
2025-01-22 06:25:52 +0600

    nothing

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ import connectDB from './config/db.js';
 import routes from './routes/index.js';
 import userRoutes from './routes/userRoutes.js';
 
+
 dotenv.config();
 connectDB();
 
@@ -20,4 +21,4 @@ app.use(express.json());
 app.use('/',routes);
 app.use('/api/users',userRoutes);
  
-export default app;
+export default app; 
